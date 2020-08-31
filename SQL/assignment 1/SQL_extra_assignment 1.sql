823cae801d6205032e2bdb109af314c00c573081
dule2401 <alka.asura@gmail.com>
2020-08-31 23:02:46 +0700

    update assignment 1

--- SQL/assignment 1/SQL_extra_assignment 1.sql
+++ SQL/assignment 1/SQL_extra_assignment 1.sql
@@ -4,18 +4,18 @@ USE `table trainee`;
 -- khai báo bảng học sinh--
 CREATE TABLE Trainee (
 Trainee_id SMALLINT UNSIGNED PRIMARY KEY,
-full_name VARCHAR(50),
+full_name VARCHAR(50) NOT NULL,
 birth_date DATE,
 gender ENUM ('male',' female','unknow'),
-training_class CHAR(20),
+training_class CHAR(20) NOT NULL,
 et_iq tinyint unsigned, 
 	check(et_iq <= 20),
 et_gmath tinyint unsigned,
 	check(et_iq <= 20),
 et_english tinyint unsigned,
 	check( et_english <=20),
-evaluation_notes Nchar(50) not null -- note đánh giá --
+evaluation_notes Nchar(50) NOT NULL -- note đánh giá --
  );
  CREATE table vti_account (
- vti_account SMALLINT UNSIGNED UNIQUE not null
+ vti_account SMALLINT UNSIGNED UNIQUE NOT NULL
 );
