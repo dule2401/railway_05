2f7f5c5c88ca562e727a0bf6c51e9457ec198b7f
dule2401 <alka.asura@gmail.com>
2020-09-01 20:42:57 +0700

    assignment 1

--- SQL/assignment 1/testing_system 2.sql
+++ SQL/assignment 1/testing_system 2.sql
@@ -33,7 +33,7 @@ CREATE TABLE `group` (
 CREATE TABLE groupaccount (
     group_id SMALLINT UNSIGNED UNIQUE,
     account_id INT UNIQUE,
-    join_date DATETIME,
+    join_date DATETIME DEFAULT NOW(),
     PRIMARY KEY (group_iD, account_id),
     FOREIGN KEY (account_id)REFERENCES `account`(account_id),
     FOREIGN KEY(group_id)REFERENCES `group`(group_id)
@@ -64,7 +64,7 @@ CREATE TABLE answer (
     answer_id SMALLINT UNSIGNED,
     content VARCHAR(50) NOT NULL CHECK(LENGTH(content)>8),
     question_id SMALLINT UNSIGNED,
-    iscorrect ENUM('đúng','sai')NOT NULL ,
+    iscorrect ENUM('đúng','sai') NOT NULL,
     PRIMARY KEY (answer_id,question_id),
     FOREIGN KEY(question_id) REFERENCES question(question_id)
 );
@@ -74,7 +74,7 @@ CREATE TABLE exam (
     `code` VARCHAR(50) UNIQUE,
     title VARCHAR(50) CHECK (LENGTH(title)>8),
     category_id SMALLINT UNSIGNED UNIQUE,
-    duration DATETIME,
+    duration DATETIME DEFAULT NOW(),
     creator_id SMALLINT UNSIGNED UNIQUE,
     create_date DATETIME DEFAULT NOW()
 );
