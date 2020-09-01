8ad44909fc9ef2ab4654be95fa3d8662cccb8754
dule2401 <alka.asura@gmail.com>
2020-09-01 20:33:46 +0700

    assignment 1

--- SQL/assignment 1/testing_system 2.sql
+++ SQL/assignment 1/testing_system 2.sql
@@ -4,30 +4,30 @@ use Testing_System;
 -- tạo bảng khai báo phòng ban--
 CREATE TABLE department (
     department_id SMALLINT UNSIGNED PRIMARY KEY,
-    department_name VARCHAR(50) UNIQUE KEY
+    department_name VARCHAR(50) UNIQUE KEY CHECK(LENGTH(department_name)>6)
 );
 -- dang tao bang chức vụ --
 CREATE TABLE position (
     position_id SMALLINT UNSIGNED PRIMARY KEY,
-    position_name VARCHAR(50)
+    position_name VARCHAR(50) UNIQUE KEY CHECK(LENGTH(position_name)>6)
 );
 -- tạo bảng khai báo tài khoản--
 CREATE TABLE `account` (
     account_id INT PRIMARY KEY,
     email VARCHAR(50) UNIQUE,
-    username VARCHAR(50) UNIQUE,
-    fullname VARCHAR(50),
+    username VARCHAR(50) UNIQUE KEY CHECK(LENGTH(username)>6),
+    fullname VARCHAR(50) NOT NULL CHECK(LENGTH(fullname)>6),
     department_id SMALLINT UNSIGNED,
     position_id SMALLINT UNSIGNED not null,
-    create_date DATETIME,
+    create_date DATETIME DEFAULT NOW(),
     FOREIGN KEY (department_id)REFERENCES department(department_id)
 );
 -- bảng khai báo group--
 CREATE TABLE `group` (
     group_id SMALLINT UNSIGNED PRIMARY KEY,
-    group_name VARCHAR(50),
+    group_name VARCHAR(50) CHECK(LENGTH(group_name)>6),
     creator_id SMALLINT UNSIGNED,
-    creat_date DATETIME
+    creat_date DATETIME DEFAULT NOW()
 );
 -- bảng khai báo nhóm--
 CREATE TABLE groupaccount (
@@ -41,7 +41,7 @@ CREATE TABLE groupaccount (
 -- loại câu hỏi --
 CREATE TABLE typequesition (
     type_id SMALLINT UNSIGNED PRIMARY KEY,
-    type_name VARCHAR(50)
+    type_name VARCHAR(50) NOT NULL CHECK (LENGTH(type_name)>8)
 );
 -- chủ đề câu hỏi --
 CREATE TABLE categoryquestion (
@@ -51,20 +51,20 @@ CREATE TABLE categoryquestion (
 -- định danh câu hỏi--
 CREATE TABLE question (
     question_id SMALLINT UNSIGNED PRIMARY KEY,
-    content VARCHAR(50),
+    content VARCHAR(50) NOT NULL CHECK(LENGTH(content)>6),
     category_id SMALLINT UNSIGNED UNIQUE,
     type_id SMALLINT UNSIGNED,
     creator_id SMALLINT UNSIGNED,
-    create_date DATETIME,
+    create_date DATETIME DEFAULT NOW(),
     FOREIGN KEY(category_id) REFERENCES categoryquestion(category_id),
     FOREIGN KEY(type_id)REFERENCES typequesition(type_id)
 );
 -- câu trả lời --
 CREATE TABLE answer (
     answer_id SMALLINT UNSIGNED,
-    content VARCHAR(50),
+    content VARCHAR(50) NOT NULL CHECK(LENGTH(content)>8),
     question_id SMALLINT UNSIGNED,
-    iscorrect ENUM('đúng','sai') not null,
+    iscorrect ENUM('đúng','sai')NOT NULL ,
     PRIMARY KEY (answer_id,question_id),
     FOREIGN KEY(question_id) REFERENCES question(question_id)
 );
@@ -72,11 +72,11 @@ CREATE TABLE answer (
 CREATE TABLE exam (
     exam_id SMALLINT UNSIGNED PRIMARY key,
     `code` VARCHAR(50) UNIQUE,
-    title VARCHAR(50),
+    title VARCHAR(50) CHECK (LENGTH(title)>8),
     category_id SMALLINT UNSIGNED UNIQUE,
     duration DATETIME,
     creator_id SMALLINT UNSIGNED UNIQUE,
-    create_date DATETIME
+    create_date DATETIME DEFAULT NOW()
 );
 -- bảng đề thi --
 CREATE TABLE examquestion (
