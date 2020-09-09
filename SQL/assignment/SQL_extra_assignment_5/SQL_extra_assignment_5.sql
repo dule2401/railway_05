b598c5e279912df41014ef11e9e0bd4275e2ddf1
dule2401 <alka.asura@gmail.com>
2020-09-10 01:11:16 +0700

    assignment

---
+++ SQL/assignment/SQL_extra_assignment_5/SQL_extra_assignment_5.sql
@@ -0,0 +1,75 @@
+-- Exercise 1: Subquery 
+-- Question 1: Viết 1 query lấy thông tin "Name" từ bảng Production.Product có name
+-- của ProductSubcategory là 'Saddles'.
+use `adventureworks`;
+SELECT 
+    P.ProductID,P.`Name`,PR.`Name`
+FROM
+    `product` P
+    join `productsubcategory` PR on P.ProductSubcategoryID =PR.ProductSubcategoryID
+WHERE
+    P.ProductSubcategoryID = (SELECT 
+            ProductSubcategoryID
+        FROM
+            `productsubcategory`
+        WHERE
+            `Name` = 'Saddles');
+            
+-- Question 2: Thay đổi câu Query 1 để lấy được kết quả sau.
+-- In this exercise you can change the previous query to deliver the following result set. 
+
+
+
+-- Question 3:
+-- Viết câu query trả về tất cả các sản phẩm có giá rẻ nhất (lowest ListPrice) và Touring
+-- Bike (nghĩa là ProductSubcategoryID = 3) 
+use `adventureworks`;
+SELECT 
+    `Name`
+FROM
+    product
+WHERE
+    StandardCost = (SELECT 
+            *
+        FROM
+            (SELECT 
+                MIN(StandardCost)
+            FROM
+                product
+            WHERE
+                ProductSubcategoryID = 3) AS min_StandardCost);
+                
+--------------------------------------------------------------------
+-- Exercise 2: JOIN nhiều bảng
+-- Question 1: Viết query lấy danh sách tên country và provicountryregionnce được lưu trong
+-- AdventureWorks2008sample database.
+
+use `adventureworks`;
+SELECT 
+    CT.`Name` AS Country, ST.`Name` AS Province
+FROM
+    `countryregion` CT
+        JOIN
+    stateprovince ST ON CT.CountryRegionCode = ST.CountryRegionCode;
+--------------------------------------------------------------------------
+-- Question 2: Tiếp tục với câu query trước và thêm điều kiện là chỉ lấy country
+-- Germany và Canada
+use `adventureworks`;
+SELECT 
+    CT.`Name` AS Country, ST.`Name` AS Province
+FROM
+    `countryregion` CT
+        JOIN
+    stateprovince ST ON CT.CountryRegionCode = ST.CountryRegionCode
+   
+   WHERE CT.`Name`= 'Germany' or CT.`Name`= 'Canada';
+   
+-------------------------------------------------------------
+-- Question 3:
+-- SalesOrderID, OrderDate and SalesPersonID. Từ bảng SalesPerson, chúng ta lấy cột
+-- BusinessEntityID (là định danh của người sales), Bonus and the SalesYTD (là đã sale
+-- được bao nhiêu người trong năm nay)
+
+
+
+
